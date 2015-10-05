9419db2855c41fa3c4426d7f7ae108cea8770eb5
Bethany Brown <bethany@digital-telepathy.com>
2015-10-05 16:41:13 -0700

    soundcloud, github section

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -14,6 +14,25 @@
        });
     },
 
+    _APICalls: function(){
+
+      SC.initialize({
+        client_id: '5fac9a802c6eb0c4933f373e2c716220'
+      });
+
+      // var UserID = 10996624;
+
+        SC.get('/playlists/5913006').then(function(playlist) {
+          var title = playlist.tracks[0].title;
+          var artist = playlist.tracks[0].title;
+          var url = playlist.tracks[0].permalink_url;
+          $("#soundcloud").append(
+            "<p><a href='"+ url +"'>"+title+"</a></p>")
+        });
+
+
+    },
+
     _getElements: function(){
       for( var key in this.selectors ) {
           this.elements[key] = $( this.selectors[key] );
@@ -25,12 +44,14 @@
     animation: function(){
       //Animation classes are triggered at certain scroll points
       var self = this;
-      var scrollTop = self.elements.window.scrollTop();
-      var windowHeight = self.elements.window.height();
-      var effectOffset = self.elements.effect.offset().top;
-      var difference = effectOffset - windowHeight;
-      if( scrollTop > difference ) {
-        self.elements.effect.addClass('visible');
+      if (self.elements.effect.length) {
+        var scrollTop = self.elements.window.scrollTop();
+        var windowHeight = self.elements.window.height();
+        var effectOffset = self.elements.effect.offset().top;
+        var difference = effectOffset - windowHeight;
+        if( scrollTop > difference ) {
+          self.elements.effect.addClass('visible');
+        }
       }
 
     },
@@ -39,6 +60,7 @@
       this._getElements();
       this._bindEvents();
       this.animation();
+      this._APICalls();
 
     }
   }; // MainScripts
