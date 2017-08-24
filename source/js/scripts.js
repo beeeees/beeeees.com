9738fc88b0d29945e37045820edcd15a881615fe
Bethany Brown <bethany@digital-telepathy.com>
2017-08-23 21:22:11 -0700

    typekit and removing unused

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -14,34 +14,6 @@
        });
     },
 
-    soundcloudAPI: function(){
-
-      SC.initialize({
-        client_id: '5fac9a802c6eb0c4933f373e2c716220'
-      });
-
-      // var UserID = 10996624;
-
-    //using experimental API call to grab reposts. if this stops working, next best thing is below
-      SC.get('/e1/users/10996624/sounds').then(function(sound) {
-        var title = sound[0].track.title;
-        var artist = sound[0].track.title;
-        var url = sound[0].track.permalink_url;
-        $("#soundcloud p").append(
-          "<a href='"+ url +"'>"+title+"</a></p>")
-      });
-
-      //grabs first song in a playlist
-
-      // SC.get('/playlists/5913006').then(function(playlist) {
-      //   var title = playlist.tracks[0].title;
-      //   var artist = playlist.tracks[0].title;
-      //   var url = playlist.tracks[0].permalink_url;
-      //   $("#soundcloud p").append(
-      //     "<a href='"+ url +"'>"+title+"</a></p>")
-      // });
-
-    },
 
     _getElements: function(){
       for( var key in this.selectors ) {
@@ -71,9 +43,7 @@
     initialize: function(){
       this._getElements();
       this._bindEvents();
-      this.animation();
-      this.soundcloudAPI();
-
+      this.animation();]
     }
   }; // MainScripts
 
